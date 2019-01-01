db01dced6f1a83357038f7ca1dd9000fc44ef65a
Filipe Gaspar <fipewmc@gmail.com>
2019-01-01 10:13:34 +0000

    criei o meu repositorio

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+	echo "<strong>Olá mundo!</strong><br/><br/>";
+	echo "Seja bem bindo ao curso de PHP7 da Hcode na <a href=https://www.udemy.com target=_blanck>Udemy</a><br/><br/>";
+	echo "<br/><pre><strong><a href=exerc02.php target=_blank>Exercicio 2</a>";
+	echo "&#09<a >Exercicio 3</a></strong></pre>";
+?>
