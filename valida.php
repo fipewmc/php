8bab9429ab95f2022618ec977191ce8d577f0b72
fipewmc <46294972+fipewmc@users.noreply.github.com>
2019-01-24 22:08:26 +0000

    Correcção de alguns erros
    coneccção com sucesso á BD

--- valida.php
+++ valida.php
@@ -12,17 +12,37 @@
 
 		if((!empty($utilizador)) AND (!empty($senha))){
 			
-			$sql = "SELECT utilizador from utilizadores WHERE utilizador = '$utilizador'";//consulta á base de dados
+			
+				//pesquisar utilizador na base de dados
+				$result_utilizador = "SELECT id, nome, email, senha FROM utilizadores WHERE utilizador = '$utilizador' LIMIT 1";//dados para pesquisa na base de dados
+				
+				$resultado_utilizador = mysqli_query($connect, $result_utilizador);//Pesquisa na base de dados
+								
+				if ($resultado_utilizador){// verifica se a variavel $resultado_utilizador tem algum valor
+					$row_utilizador = mysqli_fetch_assoc($resultado_utilizador);//obtenção do valor contido na base de dados
+					if (password_verify ($senha, $row_utilizador['senha'])){//compara a password digitada com a password guardada na base de dados
+						
+						//para desenvolvimento
+
+						header("Location: administrativo.php");//faz o redirecionamento para a página de administração do site
+					}else{
+						$_SESSION['msg'] = "Login ou Senha Incorreto";//Variavel global para enviar uma mensagem
+						header("Location: login.php");//faz o redirecionamento para a página de login
+					}
+				}
+
+				//Adicionado para testar outra dorma mais bonita de programar em testes
+			/*$sql = "SELECT utilizador from utilizadores WHERE utilizador = '$utilizador'";//consulta á base de dados
 				
 			$resultado = mysqli_query($connect, $sql);//obtenção do resultado da consulta.
 			
-			if (mysqli_num_rows($resultado) > 0){
+			if (mysqli_num_rows($resultado) > 0){//verifica se obtivemos algum resultado
 				echo "<h1>AKI estou------------</h1>";//para testes
 
 			}else{
 				$_SESSION['msg'] = "Login ou Senha Incorreto";//Variavel global para enviar uma mensagem
 				header("Location: login.php");//faz o redirecionamento para a página de login
-			}
+			}*/
 			
 			
 		}else{
