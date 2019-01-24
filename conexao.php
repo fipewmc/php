81148347f371f1a570144390e604354548d42999
fipewmc <46294972+fipewmc@users.noreply.github.com>
2019-01-24 21:56:15 +0000

    Conexao com exito
    Alteração de condiçoes

--- conexao.php
+++ conexao.php
@@ -1,9 +1,13 @@
 <?php
-	$servidor = "www.udemy.hcode.php"; //Servidor de base de dados neste exemplo está no IP 192.168.100.15 
+	$servidor = "www.udemy.hcode.php"; //Servidor de base de dados neste exemplo 
 	$utilizador = "admin"; //para testes
-	$senha = " "; //para testes neste abiente não tem password
-	$dbname = "abc"; //para testes
+	$senha = ""; //para testes neste abiente não tem password
+	$dbname = "teste"; //para testes
 
 	//criação da coneção á base de dados
-	$conn = mysqli_connect($servidor, $utilizador, $senha, $dbname);//conector para a base de dados
+	$connect = mysqli_connect($servidor, $utilizador, $senha, $dbname);//conector para a base de dados	
+	
+	if(mysqli_connect_error()){
+		echo "Falha na conexão: ".mysqli_connect_error();
+	}
 ?>
--- valida.php
+++ valida.php
@@ -1,6 +1,7 @@
 <?php
 	session_start(); //inicio de sessão
-	include_once("conexao.php");
+	require_once "conexao.php";//nesta linha está ligada ao ficheiro de conexão á base de dados
+
 	$btLogin = filter_input(INPUT_POST, 'btnLogin', FILTER_SANITIZE_STRING);//recebe o valor do formulário enviado através do método post
 		
 	if($btLogin){//Verifica se a variavel contem algum valor dentro dela
@@ -8,32 +9,22 @@
 		$utilizador = filter_input(INPUT_POST, 'utilizador', FILTER_SANITIZE_STRING); //recebe o nome do utilizador
 		$senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING);//recebe a palavra pass, password, senha
 		
+
 		if((!empty($utilizador)) AND (!empty($senha))){
-
-			//A lina de baixo serve para criptografar a password
-				echo password_hash($senha, PASSWORD_DEFAULT);// Criptografia da palavra passe, password, senha através da API do PHP
-
-			//pesquisar utilizador na base de dados
-				$result_utilizador = "SELECT id, nome, email, senha FROM utilizadores WHERE utilizador = $utilizador LIMIT 1";//dados para pesquisa na base de dados
-				echo"<br/><br/>";//linha de teste para apagar
-				var_dump($result_utilizador);//linha de teste para apagar
-				$resultado_utilizador = mysqli_query($conn, $result_utilizador);//Pesquisa na base de dados
-				echo"<br/><br/>";//linha de teste para apagar
-				var_dump($resultado_utilizador);//linha de teste para apagar
+			
+			$sql = "SELECT utilizador from utilizadores WHERE utilizador = '$utilizador'";//consulta á base de dados
 				
-				/*if $resultado_utilizador{// verifica se a variavel $resultado_utilizador tem algum valor
-					$row_utilizador = mysqli_fetch_assoc($resultado_utilizador);//obtenção do valor contido na base de dados
-					if (password_verify ($senha, $row_utilizador['senha'])){//compara a password digitada com a password guardada na base de dados
-						
-						//para desenvolvimento
+			$resultado = mysqli_query($connect, $sql);//obtenção do resultado da consulta.
+			
+			if (mysqli_num_rows($resultado) > 0){
+				echo "<h1>AKI estou------------</h1>";//para testes
 
-						header("Location: administrativo.php");//faz o redirecionamento para a página de administração do site
-					}else{
-						$_SESSION['msg'] = "Login ou Senha Incorreto";//Variavel global para enviar uma mensagem
-						header("Location: login.php");//faz o redirecionamento para a página de login
-					}
-
-				}*/
+			}else{
+				$_SESSION['msg'] = "Login ou Senha Incorreto";//Variavel global para enviar uma mensagem
+				header("Location: login.php");//faz o redirecionamento para a página de login
+			}
+			
+			
 		}else{
 			$_SESSION['msg'] = "Login ou Senha Incorreto";//Variavel global para enviar uma mensagem
 			header("Location: login.php");//faz o redirecionamento para a página de login
