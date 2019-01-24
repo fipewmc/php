f5277d679dccdf13e265a82a749f747b8bd67d3d
fipewmc <46294972+fipewmc@users.noreply.github.com>
2019-01-24 22:25:20 +0000

    Adição de formulário ao administrativo
    Adição de formulário ao administrativo para marcação de férias.

--- administrativo.php
+++ administrativo.php
@@ -1,4 +1,28 @@
 <?php
 session_start(); //inicio de sessão
-	echo "Olá, seja bem vindo";
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+
+<body>
+	<!--Inicio de bloco de código em PHP -->
+	<?php
+	echo "Olá, seja bem vindo<br>";
+	echo "<h2> ..: Marcação de Férias :.. </h2>";
+	?>
+		<form action="" method="post">
+			<label>Inicio:</label>
+			<input type="date" name="initferias" placeholder="">
+			<br/>
+			<br/>
+			<label>Fim: </label>
+			<input type="date" name="endferias" placeholder="">
+			<br/>
+			<br/>
+			<input type="submit" name="btnLogin" value="Enviar">
+		</form>
+</body>
+</html>
